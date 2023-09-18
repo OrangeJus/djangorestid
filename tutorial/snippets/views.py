55fbeedd80e86d103cd5d1b7a1fc196f6887ae7d
rafimuhm_ <balurana.baau@gmail.com>
2023-09-18 09:45:35 +0700

    Fix error in SnippetDetail

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -32,20 +32,18 @@ class SnippetList(mixins.ListModelMixin,
 class SnippetDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
-                    generics.GenericAPIView
-                ):
-    
+                    generics.GenericAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
     def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+        return self.retrieve(request, *args, **kwargs)
     
     def put(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
     
     def delete(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
 
 #Using generic class-based views
 
